f303798c88a51a249aff7c1792c8d4193f1da2d2
bojan55 <bojan.bajic55@gmail.com>
2025-12-27 13:26:55 +0100

    Create script.js and style.css files.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let textTag = document.querySelector('.section1 h1');
+let text = textTag.innerText;
+let splittedText = text.split('');
 
-textTag.classList = 'fadeMove';
+textTag.innerHTML = '';
+for(let i=0; i<splittedText.length; i++){
+    textTag.innerHTML += `<span>${splittedText[i]}</span>`;
+}
+
+
