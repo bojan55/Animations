5826f78f4e7d8a9dd22100243ba710289fc09071
bojan55 <bojan.bajic55@gmail.com>
2025-12-28 09:50:49 +0100

    Add animation on heading.

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ let splittedText = text.split('');
 
 textTag.innerHTML = '';
 for(let i=0; i<splittedText.length; i++){
+    if(splittedText[i] == " "){
+        splittedText[i] = "&nbsp;";
+    }
     textTag.innerHTML += `<span>${splittedText[i]}</span>`;
 }
 
+let i = 0;
+let interval = setInterval(() => {
+    let spans = textTag.querySelectorAll('span');
+    let singleSpan = spans[i];
 
+    singleSpan.className = 'fadeMove';
+    i++;
+
+    if(i === spans.length){
+        clearInterval(interval);
+    }
+}, 80);
